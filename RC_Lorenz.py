50cb047d37eacabe2bcffaf57a5f9686bac2a5d4
mitlaendre <mitlaendre@gmail.com>
2023-10-24 23:27:58 +0200

    Parralel befejezve, par komolyabb futi

--- RC_Lorenz.py
+++ RC_Lorenz.py
@@ -7,6 +7,7 @@ from reservoirpy import set_seed
 
 from joblib import Parallel, delayed
 
+
 def generate_data(length_train, length_test):
     x = lorenzfull(length_train + length_test, 0.01)
     x_train = x[:length_train]
@@ -53,21 +54,104 @@ def compare_3d_data(ground_truth, prediction):
 def experiment(data, reservoir_size, reservoir_lr=0.3, reservoir_sr=1.25, ridge_reg=1e-6, seed=0, warmup=100):
     x_train, y_train, x_test, y_test = data
     set_seed(seed)
-    my_esn = create_esn(reservoir_size, reservoir_lr=reservoir_lr, reservoir_sr=reservoir_sr, ridge_reg=ridge_reg, ridge_name=str(reservoir_size) + str(seed))
+    my_esn = create_esn(reservoir_size, reservoir_lr=reservoir_lr, reservoir_sr=reservoir_sr, ridge_reg=ridge_reg, ridge_name=str(reservoir_size) + str(seed) + str(reservoir_lr) + str(reservoir_sr))
     my_esn.fit(x_train, y_train, warmup=warmup)
     predictions = my_esn.run(x_test)
     error = error_func_mse(y_test, predictions)
     return error
 
-set_seed(0)
+def array_min_finder(input_array = np.array([0],dtype=object),maxthreads = 1): #output is a np.array with first element is the location vector, second is the minimum value
+    if (input_array.ndim == 1):
+        return np.array([np.array([np.argmin(input_array)]),np.amin(input_array)],dtype=object)
+    else:
+        this_dimensions_size = input_array.shape[0]
+        sub_solutions = Parallel(n_jobs=min(input_array.size,maxthreads))(delayed(array_min_finder)(input_array[i],maxthreads = max(1,(maxthreads//input_array.size))) for i in range(this_dimensions_size))
+
+        locations = np.full(this_dimensions_size,0,dtype=object)
+        minimums = np.full(this_dimensions_size,0)
+        for i in range(this_dimensions_size):
+            locations[i] = sub_solutions[i][0]
+            minimums[i] = sub_solutions[i][1]
+
+        return np.array([np.append(np.array([np.argmin(minimums)]),locations[np.argmin(minimums)]),minimums[np.argmin(minimums)]],dtype=object)
+
+def experiment_multiple(
+        data,
+        reservoir_size = np.array([100,1000,2000,5000]),
+        reservoir_lr = np.array([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]),
+        reservoir_sr = np.array([0.5,0.7,0.9,1,1.1,1.3,1.5,1.7]),
+        ridge_reg=1e-6,
+        seed = np.array([0,10,20,30,40,50,60,70,80,90]),
+        warmup: int = 100,
+        threads: int = 10):
+    errors = Parallel(n_jobs=threads)(delayed(experiment)(data = data,
+                                                          reservoir_size = reservoir_size[current_res_size].item(),
+                                                          seed = seed[current_seed].item(),
+                                                          reservoir_lr = reservoir_lr[current_reservoir_lr].item(),
+                                                          reservoir_sr = reservoir_sr[current_reservoir_sr].item()
+                                                          )
+                                                            for current_seed in range(seed.size)
+                                                            for current_res_size in range(reservoir_size.size)
+                                                            for current_reservoir_lr in range(reservoir_lr.size)
+                                                            for current_reservoir_sr in range(reservoir_sr.size)
+                                                            )
+    errors = np.array(errors).reshape(seed.size,reservoir_size.size,reservoir_lr.size,reservoir_sr.size)
+    return errors
 
 
+#multiple futi
 x_train, y_train, x_test, y_test = generate_data(3000, 1000)
-
 data = (x_train, y_train, x_test, y_test)
 
-num_res_sizes = 30;
-num_seeds = 20;
+
+
+
+Reservoir_sizes = np.array([100,1000,2000])
+Leaking_Rates = np.linspace(start = 0.1, stop = 0.9, num= 5)
+Spectral_Radiuses = np.linspace(start = 0.5, stop = 1.5, num= 5)
+Seeds = np.array([0,10,20])
+#14 err 1000,0.3,0.5
+
+Reservoir_sizes = np.array([800,900,1000,1100,1200])
+Leaking_Rates = np.linspace(start = 0.1, stop = 0.9, num= 8)
+Spectral_Radiuses = np.linspace(start = 0.5, stop = 1.5, num= 8)
+Seeds = np.array([10,20,30,40,50,60,70,80])
+#17 err  1200,0.1,0.5
+
+Reservoir_sizes = np.array([1100,1200,1300,1400,1500])
+Leaking_Rates = np.linspace(start = 0.02, stop = 0.3, num= 8)
+Spectral_Radiuses = np.linspace(start = 0.1, stop = 0.7, num= 8)
+Seeds = np.array([10,20,30,40,50,60,70,80,90,100,110,120,130,140,150])
+#12 err 1200,0.1399,0.35714
+
+Reservoir_sizes = np.array([1160,1180,1200,1220,1240])
+Leaking_Rates = np.linspace(start = 0.1, stop = 0.2, num= 8)
+Spectral_Radiuses = np.linspace(start = 0.15, stop = 0.45, num= 8)
+Seeds = np.array([10,20,30,40,50,60,70,80,90,100,110,120,130,140,150])
+#12 err 1180,0.1143,0.3643
+
+
+
+errors = experiment_multiple(data,reservoir_size=Reservoir_sizes,reservoir_lr=Leaking_Rates,reservoir_sr=Spectral_Radiuses,seed=Seeds,threads=20)
+errors1 = sum(errors)/Seeds.size
+best_setup = array_min_finder(errors1,maxthreads=10)
+print(errors1)
+print(best_setup[0])
+print(best_setup[1])
+print("Best reservoir size:")
+print(Reservoir_sizes[best_setup[0][0]])
+print("Best leaking rate:")
+print(Leaking_Rates[best_setup[0][1]])
+print("Best spectral radius:")
+print(Spectral_Radiuses[best_setup[0][2]])
+
+"""
+#sima futi
+x_train, y_train, x_test, y_test = generate_data(3000, 1000)
+data = (x_train, y_train, x_test, y_test)
+experiment((x_train, y_train, x_test, y_test), 300, seed=0)
+"""
+
 
 """errors = np.full((num_res_sizes, num_seeds),0.)
 
@@ -78,11 +162,6 @@ for res_size in range(num_res_sizes):
 plt.plot(sum(errors.transpose()), label="Error")
 plt.legend()
 plt.show()"""
-
-Parallel(n_jobs=10)(delayed(experiment)(data, (res_size + 1) * 500, seed=seed * 17) for res_size in range(num_res_sizes) for seed in range(num_seeds))
-
-#experiment((x_train, y_train, x_test, y_test), 300)
-
 
 """my_esn = create_esn(3000, reservoir_lr=0.3, reservoir_sr=1.25)
 
--- RC_orig.py
+++ RC_orig.py
@@ -2,28 +2,38 @@ import numpy as np
 import pathlib
 import reservoirpy
 
-"""teszt1 = np.full((10,2),0)
+
+
+#np.array használata
+"""
+teszt1 = np.full((10,2),0)
 teszt2 = np.array([[1,1,1],[2,2,2],[3,3,3]])
 print(teszt1)
 print("")
 print(teszt2)"""
 
 
-from joblib import Parallel, delayed
-def process(i,j):
-    return (i * i) + j
-
-results = Parallel(n_jobs=2)(delayed(process)(i,j) for i in range(10) for j in range(3))
-print(results)  # prints [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
-
-
-
-
-
-
-
-
+#Paralell és egy nagy tömb kezelése
 """
+from joblib import Parallel, delayed
+def process(i,j,k,l):
+    return i*1000 + j*100 + k*10 + l
+
+results = Parallel(n_jobs=5)(delayed(process)(i,j,k,l) for i in range(2) for j in range(3) for k in range(4) for l in range(5))
+print(results)
+print("==================================================================")
+results1 = np.array(results).reshape(2,3,4,5)
+print(results1)
+print("==================================================================")
+results2 = sum(results1) #i =0,1 adta össze
+print(results2)
+print("==================================================================")
+print(results2[2,3,4]) #i-re osszeadva, j = 2, k = 3, l = 4 elem kiirat
+"""
+
+
+#idő kezelése
+""" 
 import time
 
 start = time.time()
@@ -32,4 +42,55 @@ print(23*2.3)
 
 end = time.time()
 print(end - start)
+"""
+
+
+#félkész array min index kereső
+"""
+x = np.array([[3,2,3],[4,5,6],[7,1,9]])
+k=np.argmin(x)
+
+print(k)
+
+a = x
+ment = np.full(x.ndim,0)
+for i in range(x.ndim):
+    ment[i] = k/a.size
+    k %= a.size
+    a = a[0]
+
+print(k)
+print(ment)
+"""
+
+
+#rekurzív parralel array min index kereső
+"""
+from joblib import Parallel, delayed
+def array_min_finder(input_array = np.array([0],dtype=object)): #output is a np.array with first element is the location vector, second is the minimum value
+    #print("run with input:")
+    #print(input_array)
+    if (input_array.ndim == 1):
+        return np.array([np.array([np.argmin(input_array)]),np.amin(input_array)],dtype=object)
+    else:
+        this_dimensions_size = input_array.shape[0]
+        sub_solutions = Parallel(n_jobs=input_array.size)(delayed(array_min_finder)(input_array[i]) for i in range(this_dimensions_size))
+
+        locations = np.full(this_dimensions_size,0,dtype=object)
+        minimums = np.full(this_dimensions_size,0)
+        for i in range(this_dimensions_size):
+            locations[i] = sub_solutions[i][0]
+            minimums[i] = sub_solutions[i][1]
+
+        return np.array([np.append(np.array([np.argmin(minimums)]),locations[np.argmin(minimums)]),minimums[np.argmin(minimums)]],dtype=object)
+
+
+
+x = np.array([[[5,5,5],[5,5,5],[5,1,5]],[[5,5,5],[5,5,5],[5,0,5]]])
+
+print(x)
+print("")
+sol = array_min_index(x)
+print(sol[0])
+print(sol[1])
 """
