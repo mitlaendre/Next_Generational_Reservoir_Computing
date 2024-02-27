1033372711415af71b66f365ecbcfbcf24240f67
mitlaendre <mitlaendre@gmail.com>
2024-02-27 14:27:32 +0100

    Delete unecessary things

--- Data_Manipulation.py
+++ Data_Manipulation.py
@@ -1,17 +1,6 @@
 import numpy as np
 from joblib import Parallel, delayed
 import sympy
-def Array_Combination_to_tuple(array = np.array([],dtype=object),combination = np.array([])):
-    tuple = ()
-    for i in range(array.shape[0]):
-        tuple = tuple + (array[i][combination[i]],)
-    return tuple
-
-def Array_to_tuple(array = np.array([],dtype=object)):
-    tuple = ()
-    for i in range(array.shape[0]):
-        tuple = tuple + (array[i],)
-    return tuple
 
 def error_func_mse(x, y):
     return np.average(np.power(sum(np.square(x - y)), 0.5))
@@ -30,17 +19,6 @@ def array_min_finder(input_array = np.array([0],dtype=object),maxthreads = 1): #
 
         return (np.append(np.array([np.argmin(minimums)]),locations[np.argmin(minimums)]),minimums[np.argmin(minimums)])
 
-def multidimensional_array_special_averaging(array = np.array([],dtype=object), array_IS_this_dimension_needs_averaging = np.array([],dtype=bool)):
-    #the averaged dimensions of the array will still be there, but it will be 1 long
-    dimensions_to_Average = ()
-    for i in range(array_IS_this_dimension_needs_averaging.shape[0]):
-        if(array_IS_this_dimension_needs_averaging[i] == True):
-            dimensions_to_Average = dimensions_to_Average + (i,)
-
-    for i in dimensions_to_Average:
-        array = array.mean(axis = i,keepdims=True)
-
-    return array
 
 def data_out_of_symbols(delay = 0,dimension = 1):   #make a data, but symbols instead of floats
 
